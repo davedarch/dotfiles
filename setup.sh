752ab4fdc5982e12f7738afc2c7e43e9113f8f10
davedarch <davedarch@gmail.com>
2025-02-14 13:51:54 +0000

    Update: 2025-02-14 13:51:54

--- setup.sh
+++ setup.sh
@@ -7,7 +7,13 @@ ln -sf "$HOME/dotfiles/zsh/.zsh_github" "$HOME/.zsh_github"
 # Install required packages
 if command -v apt-get >/dev/null; then
     sudo apt-get update
-    sudo apt-get install -y zsh zsh-syntax-highlighting
+    sudo apt-get install -y zsh
+fi
+
+# Set up zsh-syntax-highlighting
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 fi
 
 # Install Oh My Zsh if not present
